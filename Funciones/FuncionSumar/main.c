58bc95e9eeec24e80fa4ba94fcedb56456bcb8ab
Matias Espindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-03-26 12:23:44 -0300

    Add files via upload

---
+++ Funciones/FuncionSumar/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumar(int , int);
+
+int main()
+{
+    int rta;
+    int x = 5;
+    int y = 7;
+    rta= sumar(x, y);
+    printf("La suma es %d \n", rta);
+    return 0;
+}
+
+int sumar(int a, int b)
+{
+    int resultado;
+    resultado = a+b;
+    return resultado;
+}
